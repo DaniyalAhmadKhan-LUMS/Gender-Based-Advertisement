f4a41ae925fdfd6ccd0f5c900bc4d623cb8aa266
DaniyalAhmad2 <22100215@lums.edu.pk>
2024-01-29 18:03:34 +0500

    Update example_live.py

--- age-and-gender/example/example_live.py
+++ age-and-gender/example/example_live.py
@@ -18,13 +18,20 @@ data.load_dnn_gender_classifier('models/dnn_gender_classifier_v1.dat')
 data.load_dnn_age_predictor('models/dnn_age_predictor_v1.dat')
 
 font = ImageFont.truetype("Acme-Regular.ttf", 20)
-
+gst_pipeline = (
+    "nvarguscamerasrc sensor-id=(int)0 ! "
+    "video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)NV12, framerate=(fraction)30/1 ! "
+    "nvvidconv ! "
+    "video/x-raw, width=(int)640, height=(int)480, format=(string)BGRx ! "
+    "videoconvert ! "
+    "appsink"
+)
 # Open a handle to the default webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)
 
 # Get the size of the video frame
 ret, frame = cap.read()
-frame_height, frame_width, _ = frame.shape
+frame_height, frame_width, _ = (640, 480,3)
 frame_center = (frame_width // 2, frame_height // 2)
 start_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 filename = f"results_{start_time}.json"
@@ -32,6 +39,11 @@ output_directory = 'results'
 output_file_path = os.path.join(output_directory, filename)
 os.makedirs(output_directory, exist_ok=True)
 
+
+frames_directory = 'frames'
+os.makedirs(frames_directory, exist_ok=True)
+
+last_saved_time = datetime.datetime.now()
 # Open a JSON file to write the results
 with open(output_file_path, 'w') as outfile:
     outfile.write('[')  # Start a JSON array
@@ -45,13 +57,19 @@ with open(output_file_path, 'w') as outfile:
 
         # Convert the captured frame to PIL Image
         img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-
+        frame = cv2.resize(frame,(640,480))
+        current_time = datetime.datetime.now()
+#        if (current_time - last_saved_time).total_seconds() > 10:
+#           frame_filename = os.path.join(frames_directory, f"frame_{current_time.strftime('%Y%m%d_%H%M%S')}.jpg")
+#           cv2.imwrite(frame_filename, frame)
+#           last_saved_time = current_time
         # Apply age and gender prediction
         result = data.predict(img)
 
         # Draw results on the frame and write to the file
         for info in result:
             if is_close_to_center(info['face'], frame_center):
+                print("Face detected and close to the center")
                 shape = [(info['face'][0], info['face'][1]), (info['face'][2], info['face'][3])]
                 draw = ImageDraw.Draw(img)
 
@@ -96,5 +114,4 @@ with open(output_file_path, 'w') as outfile:
 # When everything is done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-
 
